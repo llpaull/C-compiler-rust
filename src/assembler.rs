49e50579f1d354fcb92049cfcfb666473b6c3aac
llpaull <l.paull1302@gmail.com>
2024-05-06 16:42:45 +0200

    mult, div and mod assign

--- src/assembler.rs
+++ src/assembler.rs
@@ -13,8 +13,8 @@ pub fn assemble(ast: &parser::Program) -> String {
 fn assemble_func(func: &parser::FunDecl, res: &mut String) {
     res.push_str(&format!(".globl {}\n{}:\n", func.name, func.name));
 
-    res.push_str("pushq %rbp\n");
-    res.push_str("movq %rsp, %rbp\n");
+    res.push_str("push %rbp\n");
+    res.push_str("mov %rsp, %rbp\n");
     let mut stack = StackFrame::new();
 
     func.body
@@ -23,11 +23,11 @@ fn assemble_func(func: &parser::FunDecl, res: &mut String) {
 
     match func.body.last() {
         Some(parser::Statement::Return(_)) => {},
-        _ => res.push_str("movq $0, %rax\n"),
+        _ => res.push_str("mov $0, %rax\n"),
     }
 
-    res.push_str("movq %rbp, %rsp\n");
-    res.push_str("popq %rbp\n");
+    res.push_str("mov %rbp, %rsp\n");
+    res.push_str("pop %rbp\n");
     res.push_str("ret\n");
 }
 
@@ -41,9 +41,9 @@ fn assemble_statement(statement: &parser::Statement, stack: &mut StackFrame, res
             match opt {
                 Some(exp) => {
                     assemble_exp(exp, stack, res);
-                    res.push_str("pushq %rax\n");
+                    res.push_str("push %rax\n");
                 },
-                None => res.push_str("pushq $0\n"),
+                None => res.push_str("push $0\n"),
             }
         }
     }
@@ -56,12 +56,27 @@ fn assemble_exp(exp: &parser::Exp, stack: &mut StackFrame, res: &mut String) {
             assemble_exp(val, stack, res);
             let offset = stack.get_var(name);
             match op {
-                parser::AssignmentOp::Assign => res.push_str(&format!("movq %rax, {}(%rbp)\n", offset)),
-                parser::AssignmentOp::Plus => res.push_str(&format!("addq %rax, {}(%rbp)\n", offset)),
-                parser::AssignmentOp::Sub => res.push_str(&format!("subq %rax, {}(%rbp)\n", offset)),
-                parser::AssignmentOp::Mult => todo!(),
-                parser::AssignmentOp::Div => todo!(),
-                parser::AssignmentOp::Mod => todo!(),
+                parser::AssignmentOp::Assign => res.push_str(&format!("mov %rax, {}(%rbp)\n", offset)),
+                parser::AssignmentOp::Plus => res.push_str(&format!("add %rax, {}(%rbp)\n", offset)),
+                parser::AssignmentOp::Sub => res.push_str(&format!("sub %rax, {}(%rbp)\n", offset)),
+                parser::AssignmentOp::Mult => {
+                    res.push_str(&format!("imul {}(%rbp)\n", offset));
+                    res.push_str(&format!("mov %rax, {}(%rbp)\n", offset));
+                }
+                parser::AssignmentOp::Div => {
+                    res.push_str("mov %rax, %rcx\n");
+                    res.push_str(&format!("mov {}(%rbp), %rax\n", offset));
+                    res.push_str("cqo\n");
+                    res.push_str("idiv %rcx\n");
+                    res.push_str(&format!("mov %rax, {}(%rbp)\n", offset));
+                },
+                parser::AssignmentOp::Mod => {
+                    res.push_str("mov %rax, %rcx\n");
+                    res.push_str(&format!("mov {}(%rbp), %rax\n", offset));
+                    res.push_str("cqo\n");
+                    res.push_str("idiv %rcx\n");
+                    res.push_str(&format!("mov %rdx, {}(%rbp)\n", offset));
+               },
                 parser::AssignmentOp::BitOr => todo!(),
                 parser::AssignmentOp::BitAnd => todo!(),
                 parser::AssignmentOp::BitXor => todo!(),
@@ -77,16 +92,16 @@ fn assemble_logic_or(exp: &parser::LogicOrExp, stack: &mut StackFrame, res: &mut
         parser::LogicOrExp::LogicAnd(logic_and) => assemble_logic_and(logic_and, stack, res),
         parser::LogicOrExp::Operator(_op, l, r) => {
             assemble_logic_or(l, stack, res);
-            res.push_str("cmpq $0, %rax\n");
+            res.push_str("cmp $0, %rax\n");
             let id = unique_id();
             res.push_str(&format!("je _{}\n", id));
-            res.push_str("movq $1, %rax\n");
+            res.push_str("mov $1, %rax\n");
             let end = unique_id();
             res.push_str(&format!("jmp _{}\n", end));
             res.push_str(&format!("_{}:\n", id));
             assemble_logic_or(r, stack, res);
-            res.push_str("cmpq $0, %rax\n");
-            res.push_str("movq $0, %rax\n");
+            res.push_str("cmp $0, %rax\n");
+            res.push_str("mov $0, %rax\n");
             res.push_str("setne %al\n");
             res.push_str(&format!("_{}:\n", end));
         }
@@ -98,15 +113,15 @@ fn assemble_logic_and(exp: &parser::LogicAndExp, stack: &mut StackFrame, res: &m
         parser::LogicAndExp::BitOr(bit_or) => assemble_bit_or(bit_or, stack, res),
         parser::LogicAndExp::Operator(_op, l, r) => {
             assemble_logic_and(l, stack, res);
-            res.push_str("cmpq $0, %rax\n");
+            res.push_str("cmp $0, %rax\n");
             let id = unique_id();
             res.push_str(&format!("jne _{}\n", id));
             let end = unique_id();
             res.push_str(&format!("jmp _{}\n", end));
             res.push_str(&format!("_{}:\n", id));
             assemble_logic_and(r, stack, res);
-            res.push_str("cmpq $0, %rax\n");
-            res.push_str("movq $0, %rax\n");
+            res.push_str("cmp $0, %rax\n");
+            res.push_str("mov $0, %rax\n");
             res.push_str("setne %al\n");
             res.push_str(&format!("_{}:\n", end));
         }
@@ -118,10 +133,10 @@ fn assemble_bit_or(exp: &parser::BitOrExp, stack: &mut StackFrame, res: &mut Str
         parser::BitOrExp::BitXor(bit_xor) => assemble_bit_xor(bit_xor, stack, res),
         parser::BitOrExp::Operator(_op, l, r) => {
             assemble_bit_or(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_bit_or(r, stack, res);
-            res.push_str("popq %rcx\n");
-            res.push_str("orq %rcx, %rax\n");
+            res.push_str("pop %rcx\n");
+            res.push_str("or %rcx, %rax\n");
         }
     }
 }
@@ -131,10 +146,10 @@ fn assemble_bit_xor(exp: &parser::BitXorExp, stack: &mut StackFrame, res: &mut S
         parser::BitXorExp::BitAnd(bit_and) => assemble_bit_and(bit_and, stack, res),
         parser::BitXorExp::Operator(_op, l, r) => {
             assemble_bit_xor(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_bit_xor(r, stack, res);
-            res.push_str("popq %rcx\n");
-            res.push_str("xorq %rcx, %rax\n");
+            res.push_str("pop %rcx\n");
+            res.push_str("xor %rcx, %rax\n");
         }
     }
 }
@@ -144,10 +159,10 @@ fn assemble_bit_and(exp: &parser::BitAndExp, stack: &mut StackFrame, res: &mut S
         parser::BitAndExp::Equality(equality) => assemble_equality(equality, stack, res),
         parser::BitAndExp::Operator(_op, l, r) => {
             assemble_bit_and(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_bit_and(r, stack, res);
-            res.push_str("popq %rcx\n");
-            res.push_str("andq %rcx, %rax\n");
+            res.push_str("pop %rcx\n");
+            res.push_str("and %rcx, %rax\n");
         }
     }
 }
@@ -157,11 +172,11 @@ fn assemble_equality(exp: &parser::EqualityExp, stack: &mut StackFrame, res: &mu
         parser::EqualityExp::Rel(rel) => assemble_rel(rel, stack, res),
         parser::EqualityExp::Operator(op, l, r) => {
             assemble_equality(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_equality(r, stack, res);
-            res.push_str("popq %rcx\n");
-            res.push_str("cmpq %rax, %rcx\n");
-            res.push_str("movq $0, %rax\n");
+            res.push_str("pop %rcx\n");
+            res.push_str("cmp %rax, %rcx\n");
+            res.push_str("mov $0, %rax\n");
             match op {
                 parser::EqualityOp::Equals => res.push_str("sete %al\n"),
                 parser::EqualityOp::NotEquals => res.push_str("setne %al\n"),
@@ -175,11 +190,11 @@ fn assemble_rel(exp: &parser::RelExp, stack: &mut StackFrame, res: &mut String) 
         parser::RelExp::Shift(shift) => assemble_shift(shift, stack, res),
         parser::RelExp::Operator(op, l, r) => {
             assemble_rel(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_rel(r, stack, res);
-            res.push_str("popq %rcx\n");
-            res.push_str("cmpq %rax, %rcx\n");
-            res.push_str("movq $0, %rax\n");
+            res.push_str("pop %rcx\n");
+            res.push_str("cmp %rax, %rcx\n");
+            res.push_str("mov $0, %rax\n");
             match op {
                 parser::RelationOp::LessThan => res.push_str("setl %al\n"),
                 parser::RelationOp::LessEqual => res.push_str("setle %al\n"),
@@ -195,10 +210,10 @@ fn assemble_shift(exp: &parser::ShiftExp, stack: &mut StackFrame, res: &mut Stri
         parser::ShiftExp::Additive(additive) => assemble_additive(additive, stack, res),
         parser::ShiftExp::Operator(op, l, r) => {
             assemble_shift(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_shift(r, stack, res);
-            res.push_str("movq %rax, %rcx\n");
-            res.push_str("popq %rax\n");
+            res.push_str("mov %rax, %rcx\n");
+            res.push_str("pop %rax\n");
             match op {
                 parser::ShiftOp::LShift => res.push_str("sal %rcx, %rax\n"),
                 parser::ShiftOp::RShift => res.push_str("sar %rcx, %rax\n"),
@@ -212,17 +227,17 @@ fn assemble_additive(exp: &parser::AdditiveExp, stack: &mut StackFrame, res: &mu
         parser::AdditiveExp::Term(term) => assemble_term(term, stack, res),
         parser::AdditiveExp::Operator(op, l, r) => {
             assemble_additive(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_additive(r, stack, res);
             match op {
                 parser::AdditiveOp::Add => {
-                    res.push_str("popq %rcx\n");
-                    res.push_str("addq %rcx, %rax\n");
+                    res.push_str("pop %rcx\n");
+                    res.push_str("add %rcx, %rax\n");
                 }
                 parser::AdditiveOp::Sub => {
-                    res.push_str("movq %rax, %rcx\n");
-                    res.push_str("popq %rax\n");
-                    res.push_str("subq %rcx, %rax\n");
+                    res.push_str("mov %rax, %rcx\n");
+                    res.push_str("pop %rax\n");
+                    res.push_str("sub %rcx, %rax\n");
                 }
             }
         }
@@ -234,25 +249,25 @@ fn assemble_term(term: &parser::Term, stack: &mut StackFrame, res: &mut String) 
         parser::Term::Factor(factor) => assemble_factor(factor, stack, res),
         parser::Term::Operator(op, l, r) => {
             assemble_term(l, stack, res);
-            res.push_str("pushq %rax\n");
+            res.push_str("push %rax\n");
             assemble_term(r, stack, res);
             match op {
                 parser::TermOp::Mult => {
-                    res.push_str("popq %rcx\n");
-                    res.push_str("imulq %rcx, %rax\n");
+                    res.push_str("pop %rcx\n");
+                    res.push_str("imul %rcx, %rax\n");
                 }
                 parser::TermOp::Div => {
-                    res.push_str("movq %rax, %rcx\n");
-                    res.push_str("popq %rax\n");
+                    res.push_str("mov %rax, %rcx\n");
+                    res.push_str("pop %rax\n");
                     res.push_str("cqo\n");
-                    res.push_str("idivq %rcx\n");
+                    res.push_str("idiv %rcx\n");
                 }
                 parser::TermOp::Mod => {
-                    res.push_str("movq %rax, %rcx\n");
-                    res.push_str("popq %rax\n");
+                    res.push_str("mov %rax, %rcx\n");
+                    res.push_str("pop %rax\n");
                     res.push_str("cqo\n");
-                    res.push_str("idivq %rcx\n");
-                    res.push_str("movq %rdx, %rax\n");
+                    res.push_str("idiv %rcx\n");
+                    res.push_str("mov %rdx, %rax\n");
                 }
             }
         }
@@ -261,23 +276,23 @@ fn assemble_term(term: &parser::Term, stack: &mut StackFrame, res: &mut String) 
 
 fn assemble_factor(factor: &parser::Factor, stack: &mut StackFrame, res: &mut String) {
     match factor {
-        parser::Factor::Integer(i) => res.push_str(&format!("movq ${}, %rax\n", i)),
+        parser::Factor::Integer(i) => res.push_str(&format!("mov ${}, %rax\n", i)),
         parser::Factor::Paren(exp) => assemble_exp(exp, stack, res),
         parser::Factor::Operator(op, exp) => {
             assemble_factor(exp, stack, res);
             match op {
-                parser::FactorOp::Negate => res.push_str("negq %rax\n"),
-                parser::FactorOp::BitNot => res.push_str("notq %rax\n"),
+                parser::FactorOp::Negate => res.push_str("neg %rax\n"),
+                parser::FactorOp::BitNot => res.push_str("not %rax\n"),
                 parser::FactorOp::LogicalNot => {
-                    res.push_str("cmpq $0, %rax\n");
-                    res.push_str("movq $0, %rax\n");
+                    res.push_str("cmp $0, %rax\n");
+                    res.push_str("mov $0, %rax\n");
                     res.push_str("sete %al\n");
                 }
             }
         }
         parser::Factor::Variable(name) => {
             let offset = stack.get_var(name);
-            res.push_str(&format!("movq {}(%rbp), %rax\n", offset));
+            res.push_str(&format!("mov {}(%rbp), %rax\n", offset));
         },
     }
 }
